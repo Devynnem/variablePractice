52b14540491202f1a2dfc007d346fa4a1b65587c
Devynne Marshall <devynnem@gmail.com>
2022-12-04 16:11:43 -0700

    add console.log

--- variable.js
+++ variable.js
@@ -9,3 +9,12 @@ var age = "31"
 var depositPaid = "true"
 var happy = "true"
 var hasDogs = "true"
+
+email = "swimmingdudeduh@aol.com"
+username = "Dmarsh"
+
+console.log(email)
+console.log(temperature)
+console.log(age)
+console.log(username, numberOflikes)
+
