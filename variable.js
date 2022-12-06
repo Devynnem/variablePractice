5252bd9683f3ecc0039f622d4c4b827273c8f4d3
Devynne Marshall <devynnem@gmail.com>
2022-12-05 17:43:55 -0700

    update my variablePractice assignment

--- variable.js
+++ variable.js
@@ -2,13 +2,13 @@ var email = "devynnem@gmail.com"
 var username = "mdevynne"
 var placeOfbirth = "Wheat Ridge, CO"
 
-var temperature = "55"
-var numberOflikes = "22"
-var age = "31"
+var temperature = 55
+var numberOflikes = 22
+var age = 31
 
-var depositPaid = "true"
-var happy = "true"
-var hasDogs = "true"
+var depositPaid = true
+var happy = true
+var hasDogs = true
 
 email = "swimmingdudeduh@aol.com"
 username = "Dmarsh"
